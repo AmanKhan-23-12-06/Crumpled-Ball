7be568d6634917c426272a7924f79a17bb606b7e
AmanKhan-23-12-06 <75366027+AmanKhan-23-12-06@users.noreply.github.com>
2021-05-03 15:44:57 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,28 @@
+class Paper {
+    constructor (x,y,r) {
+        var option = {
+            isStatic : false,
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.circle(x, y, r,option);
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.image = loadImage("paper.png")
+        World.add(world,this.body)
+
+    
+    }
+
+    display() {
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);
+        pop(); 
+    }
+}
